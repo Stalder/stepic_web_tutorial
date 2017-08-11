a84279a691542a04df9ef90f9b04ad6d90516d6a
Stalder <serg_lavinov@inbox.ru>
2017-08-11 23:34:23 +0300

    Stepik les 1.9. Attempt №1

---
+++ hello.py
@@ -0,0 +1,10 @@
+def wsgi_application(environ, start_response):
+
+    body = '\n'.join(environ['QUERY_STRING'].split('&'))
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return [body]
